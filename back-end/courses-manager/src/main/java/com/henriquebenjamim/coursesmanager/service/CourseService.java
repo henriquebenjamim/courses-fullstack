30e2603963816e020a588e19979e3a98cddaab23
henriquebenjamim <henriiqqq@gmail.com>
2024-01-08 18:01:33 -0300

    Feat: added exception folder and getById method

--- back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/service/CourseService.java
+++ back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/service/CourseService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.henriquebenjamim.coursesmanager.model.Course;
 import com.henriquebenjamim.coursesmanager.repository.CourseRepository;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Validated
 @Service
@@ -20,5 +24,10 @@ public class CourseService {
 
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  public Course findById(@PathVariable @NotNull @Positive Long id) {
+    return courseRepository.findById(id)
+    .orElseThrow();
   }
 }
