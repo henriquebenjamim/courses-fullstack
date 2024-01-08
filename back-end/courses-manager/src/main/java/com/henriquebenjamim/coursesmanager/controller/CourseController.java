1ebbe0022efe6099516a2bea6f080a4b9c2d8323
henriquebenjamim <henriiqqq@gmail.com>
2024-01-08 18:15:32 -0300

    Feat: added other methods on CourseController like post,put,delete

--- back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/controller/CourseController.java
+++ back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/controller/CourseController.java
@@ -2,13 +2,24 @@ package com.henriquebenjamim.coursesmanager.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.henriquebenjamim.coursesmanager.model.Course;
 import com.henriquebenjamim.coursesmanager.service.CourseService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Validated
 @RestController
@@ -24,5 +35,27 @@ public class CourseController {
   @GetMapping
   public List<Course> list() {
     return courseService.list();
+  }
+  
+  @GetMapping("/{id}")
+  public Course findById(@PathVariable @NotNull @Positive Long id) {
+    return courseService.findById(id);
+  }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody @Valid Course course) {
+    return courseService.create(course);
+  }
+
+  @PutMapping("/{id}")
+  public Course update(@PathVariable Long id, @RequestBody @Valid Course course) {
+    return courseService.update(id, course);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(code = HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable @NotNull @Positive Long id) {
+    courseService.delete(id);
   }
 }
