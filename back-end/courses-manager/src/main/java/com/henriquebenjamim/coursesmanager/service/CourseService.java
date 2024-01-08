f2fb47576989b68ab9e3b4e111e1551d53b4fa59
henriquebenjamim <henriiqqq@gmail.com>
2024-01-08 18:05:23 -0300

    Feat: added other methods on service CourseService layer like create/update/delete

--- back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/service/CourseService.java
+++ back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/service/CourseService.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.henriquebenjamim.coursesmanager.exception.RecordNotFoundException;
 import com.henriquebenjamim.coursesmanager.model.Course;
 import com.henriquebenjamim.coursesmanager.repository.CourseRepository;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -29,5 +31,23 @@ public class CourseService {
   public Course findById(@PathVariable @NotNull @Positive Long id) {
     return courseRepository.findById(id)
     .orElseThrow();
+  }
+
+  public Course create(@Valid Course course) {
+    return courseRepository.save(course);
+  }
+
+  public Course update(@NotNull @Positive Long id, @Valid Course course) {
+    return courseRepository.findById(id)
+      .map(recordFound -> {
+        recordFound.setName(course.getName());
+        recordFound.setCategory(course.getCategory());
+        return courseRepository.save(recordFound);
+      }).orElseThrow(() -> new RecordNotFoundException(id));
+  }
+
+  public void delete(@PathVariable @NotNull @Positive Long id) {
+    courseRepository.delete(courseRepository.findById(id)
+    .orElseThrow(() -> new RecordNotFoundException(id)));
   }
 }
