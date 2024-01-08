68fd8c6b15bb4d28242462f209a571f1b67723c6
henriquebenjamim <henriiqqq@gmail.com>
2024-01-08 17:54:41 -0300

    Feat: added validation on Course Model

--- back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/model/Course.java
+++ back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/model/Course.java
@@ -1,22 +1,45 @@
 package com.henriquebenjamim.coursesmanager.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
   
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @NotBlank
+  @NotNull
+  @Length(min = 5, max = 100)
+  @Column(length = 100, nullable = false)
   private String name;
 
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Back-end|Front-end")
+  @Column(length = 10, nullable = false)
   private String category;
+
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Ativo|Inativo")
+  @Column(length = 10, nullable = false)
+  private String status = "Ativo";
 }
