ed108a3b5b27d24e72970b425d3d8c18e891354b
henriquebenjamim <henriiqqq@gmail.com>
2024-01-10 20:29:09 -0300

    Feat: added dto and mappers to work with course entity

--- back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/controller/CourseController.java
+++ back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/controller/CourseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.henriquebenjamim.coursesmanager.model.Course;
+import com.henriquebenjamim.coursesmanager.dto.CourseDTO;
 import com.henriquebenjamim.coursesmanager.service.CourseService;
 
 import jakarta.validation.Valid;
@@ -25,7 +25,7 @@ import jakarta.validation.constraints.Positive;
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
-  
+
   private final CourseService courseService;
 
   public CourseController(CourseService courseService) {
@@ -33,23 +33,23 @@ public class CourseController {
   }
 
   @GetMapping
-  public List<Course> list() {
+  public List<CourseDTO> list() {
     return courseService.list();
   }
-  
+
   @GetMapping("/{id}")
-  public Course findById(@PathVariable @NotNull @Positive Long id) {
+  public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
     return courseService.findById(id);
   }
 
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
-  public Course create(@RequestBody @Valid Course course) {
+  public CourseDTO create(@RequestBody @Valid @NotNull CourseDTO course) {
     return courseService.create(course);
   }
 
   @PutMapping("/{id}")
-  public Course update(@PathVariable Long id, @RequestBody @Valid Course course) {
+  public CourseDTO update(@PathVariable Long id, @RequestBody @Valid @NotNull CourseDTO course) {
     return courseService.update(id, course);
   }
 
---
+++ back-end/courses-manager/src/main/java/com/henriquebenjamim/coursesmanager/dto/mapper/CourseMapper.java
@@ -0,0 +1,35 @@
+package com.henriquebenjamim.coursesmanager.dto.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.henriquebenjamim.coursesmanager.dto.CourseDTO;
+import com.henriquebenjamim.coursesmanager.model.Course;
+
+
+
+@Component
+public class CourseMapper {
+  
+  public CourseDTO toDTO(Course course) {
+    if (course == null) {
+      return null;
+    }
+    return new CourseDTO(course.getId(), course.getName(), course.getCategory());
+  }
+
+  public Course toEntity(CourseDTO courseDTO) {
+
+    if (courseDTO == null) {
+      return null;
+    }
+
+    Course course = new Course();
+    if (courseDTO.id() != null) {
+      course.setId(courseDTO.id());
+    }
+    course.setName(courseDTO.name());
+    course.setCategory(courseDTO.category());
+    course.setStatus("Ativo");
+    return course;
+  }
+}
